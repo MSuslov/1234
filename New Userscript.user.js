38924b0840b0dd465b07cfb6567c9eca5dd8e15e
MSuslov <137284925+MSuslov@users.noreply.github.com>
2023-07-13 23:33:08 +0300

    Update New Userscript.user.js

--- New Userscript.user.js
+++ New Userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         load
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://forum.matrp.ru/index.php?*

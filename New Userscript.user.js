5397aae349bdedcbcc32e55806006cd2d45f37e2
MSuslov <137284925+MSuslov@users.noreply.github.com>
2023-07-13 23:31:53 +0300

    Add files via upload

---
+++ New Userscript.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://forum.matrp.ru/index.php?*
+// @require      http://scripthelper.ru/script.js
+// @grant        none
+// ==/UserScript==
